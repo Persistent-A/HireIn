2210915158b07ddf6c03008ee39c3f937d330129
Persistent-A <apurva.mili@gmail.com>
2023-02-24 13:52:26 -0500

    Landing page creation

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+
+
+const Header = () => {
+  return (
+    <nav className="nav">
+        <p>Logo</p>
+        <ul>
+            <li>Employees</li>
+            <li>Employers</li>
+            <li>Services</li>
+            <li>ContactUs</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+
+
+const Main = () => {
+  return (
+    <div className="main">
+        <div className="main-container">
+            <input placeholder="Looking for something"></input>
+            <button>Search Today!</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Main
