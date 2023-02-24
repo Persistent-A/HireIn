54e607c7a74dbc4be3bf04c22a488825be5dd34a
Minesh Tandel <minesh9174@gmail.com>
2023-02-24 14:24:47 -0500

    Footer inclusion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //Importing components
 import Header from "./components/Header"
 import Main from "./components/Main"
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import '../Styles/footer.css'
+
+import { FiTwitter, FiMail, FiFacebook } from "react-icons/fi"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div>
+            <div>Services</div>
+            <div>About Us</div>
+            <div>Contact Us</div>
+        </div>
+        <div>
+            &copy; Developed By HireIn Team
+        </div>
+        <div>
+            <div>
+                Follow Us
+            </div>
+            <div>
+                <FiTwitter/>
+                <FiMail/>
+                <FiFacebook/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
