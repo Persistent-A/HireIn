7e0bf3cae0629f014e0dece22517dced4e36178b
Persistent-A <apurva.mili@gmail.com>
2023-02-25 14:44:26 -0500

    Revert "Excluded Firebase"
    This reverts commit 74bff56b372902d0ab8de5b5c33eb94a353680a1.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 //Importing components
 import Header from "./components/Header"
 import Main from "./components/Main"
-// import Auth from "./components/Auth"
 import Footer from "./components/Footer";
-
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
-      {/* <Auth /> */}
       <Footer/>
     </div>
   );
