24c02ddcc24a417c80600829ab0c42552e775e76
Persistent-A <apurva.mili@gmail.com>
2023-02-25 19:48:39 -0500

    retrieving previous commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer";
-import AllFeaturEplyee from "./components/AllFeaturEplyee"
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
-      <AllFeaturEplyee />
       <Footer/>
     </div>
   );
