1f9b96084e1958cb3917d75d4c288a6a68468462
Persistent-A <apurva.mili@gmail.com>
2023-02-25 21:39:02 -0500

    Featured Employee Section Inclusion

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer";
+import AllFeaturedEmployee from "./components/AllFeaturedEmployee";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <AllFeaturedEmployee/>
       <Footer/>
     </div>
   );
---
+++ src/components/FeaturedEmp.js
@@ -0,0 +1,17 @@
+//import styles
+import '../Styles/featuredEmployee.css'
+
+const FeaturedEmp = (props) => {
+  return (
+    <div className="featuredEmployeeContainer">
+        <div>
+            <p>{props.name}</p>
+            <p>{props.ratings}</p>
+        </div>
+        <div>{props.skills}</div>
+        <div>{props.about}</div>
+    </div>
+  )
+}
+
+export default FeaturedEmp
