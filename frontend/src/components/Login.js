0690da7b4a3a4cb51f3db5eb70bce81ee8634b60
Persistent-A <apurva.mili@gmail.com>
2023-03-10 16:05:47 -0500

    Backend Initialization

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <div className="form-container">
+        <p>Login/SignUp</p>
+        <form>
+            <label>Email:</label>
+            <input></input>
+        </form>
+      
+    </div>
+  )
+}
+
+export default Login
