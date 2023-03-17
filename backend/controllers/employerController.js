559e0d7eb54b747e5697e404b951ccb8e6571563
Minesh Tandel <minesh9174@gmail.com>
2023-03-17 13:42:47 -0400

    employer can update his data

--- backend/controllers/employerController.js
+++ backend/controllers/employerController.js
@@ -24,10 +24,13 @@ const registerEmployer = asyncHandler(async(req, res) => {
 
     const employer = await Employer.create({
         first_name, 
-        last_name, 
+        last_name,
+        age: null,
+        gender: null, 
         phone, 
         email, 
-        password: hashedPassword
+        password: hashedPassword,
+        address: {}
     })
 
     if(!employer){
@@ -39,8 +42,11 @@ const registerEmployer = asyncHandler(async(req, res) => {
         _id: employer.id,
         first_name: employer.first_name,
         last_name: employer.last_name,
+        age: employer.age,
+        gender: employer.gender, 
         phone: employer.phone,
         email: employer.email,
+        address: employer.address,
         token: await generateToken(employer.id)
     })
 })
@@ -61,15 +67,33 @@ const loginEmployer = asyncHandler(async(req, res) => {
             _id: employer.id,
             first_name: employer.first_name,
             last_name: employer.last_name,
+            age: employer.age,
+            gender: employer.gender, 
             phone: employer.phone,
             email: employer.email,
+            address: employer.address,
             token: await generateToken(employer.id)
         })
     }else{
         res.status(400)
         throw new Error("User not found")
     }
-})
+    })
+
+    const updateEmployer = asyncHandler(async(req, res) => {
+        console.log(req.body)
+        const employer = await Employer.findById(req.user._id)
+        if (!employer) {
+            res.status(401)
+            throw new Error('Not Authorised')
+        }
+        else {
+            const updatedEmployer = await Employer.findByIdAndUpdate(req.user._id, req.body, {
+                new: true
+            })
+            res.status(200). json(updatedEmployer)
+        }
+    })
 
 const generateToken = async(id) => {
     return await jwt.sign({id}, process.env.JWT_SECRET, {
@@ -77,4 +101,4 @@ const generateToken = async(id) => {
     })
 }
 
-module.exports = {registerEmployer, loginEmployer};
+module.exports = {registerEmployer, loginEmployer, updateEmployer};
--- frontend/src/components/EmployerDashboard.js
+++ frontend/src/components/EmployerDashboard.js
@@ -1,22 +1,52 @@
 import "../Styles/employerDashboard.css"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import EmployerProfile from "./EmployerProfile"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { reset, logout } from "../features/auth/authSlice"
 
 const EmployerDashboard = () => {
 
-    // const { employee, employer, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
-    console.log(employer)
+    const { employer, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const Logout = () => {
+        dispatch(logout())  
+        dispatch(reset())
+        navigate('/')
+    }
+
+
+    useEffect( () => {
+        if(isError){
+          console.log(message)
+        }
+    
+        if(!employer) {
+          navigate('/')
+        }
+    
+        return () => {
+          dispatch(reset())
+        }
+      }, [employer, navigate, message, isError, isSuccess, dispatch])
+
+      if(isLoading) {
+        <h1>loading......</h1>
+      }
   
     return (
     <div className="employer-dashboard">
         <div>
-            <p>Welcome</p>
+            <p>Welcome {employer.first_name}</p>
             <button>Account</button>
             <button>Search Services</button>
-            <button>Logout</button>
+            <button onClick={Logout}>Logout</button>
         </div>
 
-        <div>
+        <div className="employer-dashboard-extention">
             <EmployerProfile />
         </div>
       
