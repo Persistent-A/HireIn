1e70d66dfcafb4cf023931242f26e53f29e11b91
Persistent-A <apurva.mili@gmail.com>
2023-03-25 16:57:32 -0400

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,14 @@ app.use('/employers', require('./routes/employerRoutes'))
 app.use('/employees', require('./routes/employeeRoutes'))
 app.use('/forgot-pass', require('./routes/forgotPass'))
 app.use('/admin', require('./routes/adminRoutes'))
-app.use(errorHandler)
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please Activate Production'))
+}
+
+app.use(errorHandler)
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
