cf5f111cc31c7badea8c93fcb075cd32bb599dd4
Persistent-A <apurva.mili@gmail.com>
2023-05-12 13:11:18 -0400

    Appointment Route creation

---
+++ backend/controllers/appointmentController.js
@@ -0,0 +1,44 @@
+const asyncHandler = require("express-async-handler");
+const Employee = require("../models/employeeModel");
+const Employer = require("../models/employerModel");
+const Appointments = require("../models/appointmentModel");
+const Engagements = require("../models/engagementsModel");
+
+const requestEmployee = asyncHandler(async (req, res) => {
+  const { employee_id, employer_id } = req.body;
+  const employee = await Employee.findById(employee_id);
+  const employer = await Employer.findById(employer_id);
+  if (!employee) {
+    res.status(400);
+    throw new Error("Error retrieving Employees information");
+  }
+  if (!employer) {
+    res.status(400);
+    throw new Error("Error retrieving employee data");
+  }
+  const appointment = await Appointments.create({
+    appointment_booktime: Date.now(),
+  });
+  const engagement = await Engagements.create({
+    employer_id: req.employer._id,
+    employee_id: employee._id,
+    appointment_id: appointment._id,
+  });
+  if (engagement) {
+    res.status(201).json({ message: "Appointment booked" });
+  } else {
+    res.status(400);
+    throw new Error("Error Booking an Appointment");
+  }
+});
+
+const getAppointments = asyncHandler(async (req, res) => {
+  const engagement = await Engagements.find({ employee: req.employee._id })
+    .populate("employer_id", "first_name last_name email")
+    .populate("employer_id", "first_name last_name email")
+    .populate("appointment_id")
+    .exec();
+  res.status(200).json(engagement);
+});
+
+module.exports = { requestEmployee, getAppointments };
--- backend/routes/employerRoutes.js
+++ backend/routes/employerRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const protect = require('../middlewares/protect')
-const {registerEmployer, loginEmployer, updateEmployer} = require('../controllers/employerController')
-const { getEmployees } = require('../controllers/employeeController')
+const {registerEmployer, loginEmployer, updateEmployer } = require('../controllers/employerController')
+const {requestEmployee} = require('../controllers/appointmentController')
+const { getEmployees} = require('../controllers/employeeController')
 
 
 // Register and Login Routes for employer
@@ -10,5 +11,6 @@ router.route('/register/').post(registerEmployer)
 router.route('/login/').post(loginEmployer)
 router.route('/update/').put(protect, updateEmployer)
 router.route('/search/employee/:specialization').get(getEmployees)
+router.route('/hire/').post(requestEmployee)
 
 module.exports = router;
