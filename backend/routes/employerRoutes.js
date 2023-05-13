77244d7f545eb52410dbc5b635d00fa1f2d14036
Minesh Tandel <minesh9174@gmail.com>
2023-05-13 09:40:23 -0400

    Get Appointment Route integration

--- backend/routes/employerRoutes.js
+++ backend/routes/employerRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const protect = require('../middlewares/protect')
 const {registerEmployer, loginEmployer, updateEmployer} = require('../controllers/employerController')
-const { getEmployees } = require('../controllers/employeeController')
+const { getEmployees, getAppoitments } = require('../controllers/employeeController')
 
 
 // Register and Login Routes for employer
@@ -10,5 +10,6 @@ router.route('/register/').post(registerEmployer)
 router.route('/login/').post(loginEmployer)
 router.route('/update/').put(protect, updateEmployer)
 router.route('/search/employee/:specialization').get(getEmployees)
+router.route('/get-appointments/').get(getAppoitments)
 
 module.exports = router;
