836fa02d294e4514ad64add3d9de91d7efb80ed5
lakshmanbadiga <lakshmanbadiga7@gmail.com>
2023-05-28 18:35:08 -0400

    Admin can delete user accoount

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -4,11 +4,15 @@ const {
   loginAdmin,
   addService,
   getServices,
+  getUser,
+  deleteUserAccount
 } = require("../controllers/adminController");
 
 // Login Routes for user
 router.route("/login/").post(loginAdmin);
 router.route("/add-service/").post(addService);
 router.route("/get-services/").get(getServices);
+router.route("/get-user/").post(getUser);
+router.route("/delete-user/:userId").delete(deleteUserAccount);
 
 module.exports = router;
--- frontend/src/components/Inquiries.js
+++ frontend/src/components/Inquiries.js
@@ -14,9 +14,16 @@ const Inquiries = () => {
     getInquiries();
   });
   return (
-    <div>
+    <div className="inquiry-cards overflow-auto">
       {inquiries &&
-        inquiries.map((inquiry) => <div key={inquiry._id}>{inquiry.name}</div>)}
+        inquiries.map((inquiry) => (
+          <div key={inquiry._id} className="mb-5 rounded-3 border border-primary p-3">
+            <p>Name: {inquiry.name}</p>
+            <p>Email: {inquiry.email}</p>
+            <p>Phone: {inquiry.phone}</p>
+            <p>Message: {inquiry.message}</p>
+          </div>
+        ))}
     </div>
   );
 };
