9e8aa771c93079de9710e24f6a6197668dd25ac9
lakshmanbadiga <lakshmanbadiga7@gmail.com>
2023-05-27 21:47:20 -0400

    Employee Dashboard styling and functional inclusion

--- frontend/src/components/EmployeeAppointments.js
+++ frontend/src/components/EmployeeAppointments.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import "../Styles/employeeAppointment.css";
 
 const EmployeeAppointments = () => {
   const { employee } = useSelector((state) => state.auth);
@@ -19,46 +20,48 @@ const EmployeeAppointments = () => {
   }, []);
 
   return (
-    <div className="employer-appointment-container">
+    <>
       <h2 className="employer-appointment-title">Appointments</h2>
-      {appointments ? (
-        appointments.map((appointment) => (
-          <div
-            key={appointment.appointment_id}
-            className="employer-appointment-card"
-          >
-            <div className="employer-appointment-cardHeader">
-              <h4 className="employer-appointment-cardHeaderText">
-                {appointment.employer.first_name}{" "}
-                {appointment.employer.last_name}
-              </h4>
+      <div className="employer-appointment-container">
+        {appointments ? (
+          appointments.map((appointment) => (
+            <div
+              key={appointment.appointment_id}
+              className="employer-appointment-card"
+            >
+              <div className="employer-appointment-cardHeader">
+                <h4 className="employer-appointment-cardHeaderText">
+                  {appointment.employer.first_name}{" "}
+                  {appointment.employer.last_name}
+                </h4>
+              </div>
+              <div className="employer-appointment-cardBody">
+                <p className="employer-appointment-cardBodyText">
+                  Age: {appointment.employer.age}
+                </p>
+                <p className="employer-appointment-cardBodyText">
+                  Gender: {appointment.employer.gender}
+                </p>
+                <p className="employer-appointment-cardBodyText">
+                  Email: {appointment.employer.email}
+                </p>
+                <p className="employer-appointment-cardBodyText">
+                  Phone: {appointment.employer.phone}
+                </p>
+                <p className="employer-appointment-cardBodyText">
+                  Address: {appointment.employer.address.apt}{" "}
+                  {appointment.employer.address.street},{" "}
+                  {appointment.employer.address.city}{" "}
+                  {appointment.employer.address.postal}
+                </p>
+              </div>
             </div>
-            <div className="employer-appointment-cardBody">
-              <p className="employer-appointment-cardBodyText">
-                Age: {appointment.employer.age}
-              </p>
-              <p className="employer-appointment-cardBodyText">
-                Gender: {appointment.employer.gender}
-              </p>
-              <p className="employer-appointment-cardBodyText">
-                Email: {appointment.employer.email}
-              </p>
-              <p className="employer-appointment-cardBodyText">
-                Phone: {appointment.employer.phone}
-              </p>
-              <p className="employer-appointment-cardBodyText">
-                Address: {appointment.employer.address.apt}{" "}
-                {appointment.employer.address.street},{" "}
-                {appointment.employer.address.city}{" "}
-                {appointment.employer.address.postal}
-              </p>
-            </div>
-          </div>
-        ))
-      ) : (
-        <div>No appointments</div>
-      )}
-    </div>
+          ))
+        ) : (
+          <div className="no-appointments">No appointments</div>
+        )}
+      </div>
+    </>
   );
 };
 
--- frontend/src/components/EmployeeDashboard.js
+++ frontend/src/components/EmployeeDashboard.js
@@ -1,70 +1,77 @@
-import "../Styles/employerDashboard.css"
-import { useSelector, useDispatch } from "react-redux"
-import EmployeeProfile from "./EmployeeProfile"
-import EmployeeAppointments from "./EmployeeAppointments"
-import { useEffect, useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { reset, logout } from "../features/auth/authSlice"
-import axios from 'axios'
+import "../Styles/employerDashboard.css";
+import { useSelector, useDispatch } from "react-redux";
+import EmployeeProfile from "./EmployeeProfile";
+import EmployeeAppointments from "./EmployeeAppointments";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { reset, logout } from "../features/auth/authSlice";
+import axios from "axios";
+
+import { GrContactInfo } from "react-icons/gr"
+import { AiOutlineSchedule } from "react-icons/ai"
+import { MdLogout } from "react-icons/md"
 
 const EmployeeDashboard = () => {
-    const { employee, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
-    const [services, setServices] = useState()
+  const { employee, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+  const [services, setServices] = useState();
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()  
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  // dispatch(logout())
 
-    // dispatch(logout())  
+  const Logout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/employee-register");
+  };
 
-    const Logout = () => {
-        dispatch(logout())  
-        dispatch(reset())
-        navigate('/employee-register')
+  useEffect(() => {
+    const getServices = async () => {
+      const response = await axios.get("/admin/get-services/");
+      setServices(response.data);
+    };
+    getServices();
+
+    if (isError) {
+      console.log(message);
     }
-    
-    useEffect(() => {      
-        const getServices = async() => {
-            const response = await axios.get('/admin/get-services/')
-            setServices(response.data)
-        }
-        getServices()
 
-        if(isError){
-          console.log(message)
-        }
-    
-        if(!employee) {
-          navigate('/employee-register')
-        }
-    
-        return () => {
-          dispatch(reset())
-        }
-      }, [navigate, employee, message, isError, isSuccess, dispatch])
+    if (!employee) {
+      navigate("/employee-register");
+    }
 
-      if(isLoading) {
-        <h1>loading......</h1>
-      }
+    return () => {
+      dispatch(reset());
+    };
+  }, [navigate, employee, message, isError, isSuccess, dispatch]);
+
+  if (isLoading) {
+    <h1>loading......</h1>;
+  }
   return (
     <div className="employer-dashboard">
-        <div>
-            <p>Welcome {employee ? employee.first_name : ""}</p>
-            <Link to="/employee-dashboard/account/">Account</Link>
-            <Link to="/employee-dashboard/appointments/">Appoointments</Link>
-            <button onClick={Logout}>Logout</button>
-        </div>
+      <div className="employer-dashboard-content">
+        <p>Welcome {employee ? employee.first_name : ""}</p>
+        <div><GrContactInfo/><Link to="/employee-dashboard/account/" className="employer-dashboard-link">Account</Link></div>
+        <div><AiOutlineSchedule/><Link to="/employee-dashboard/appointments/" className="employer-dashboard-link">Appointments</Link></div>
+        <button onClick={Logout}><MdLogout/>Logout</button>
+      </div>
 
-        <div className="employer-dashboard-extention">
-          <Routes>
-            <Route path="/account/" element={<EmployeeProfile services={services}/>}/>
-            <Route path="/appointments/*" element={<EmployeeAppointments/>}/>
-          </Routes>
-        </div>
-      
+      <div className="employer-dashboard-extention">
+        <Routes>
+          <Route
+            path="/account/"
+            element={<EmployeeProfile services={services} />}
+          />
+          <Route path="/appointments/*" element={<EmployeeAppointments />} />
+        </Routes>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EmployeeDashboard
+export default EmployeeDashboard;
