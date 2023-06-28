1a63ab4dee027f33899c136fc1f80afcf1ec12e7
Minesh Tandel <minesh9174@gmail.com>
2023-06-28 05:59:53 -0400

    Contact us page redesigned

--- frontend/src/components/ContactUs.js
+++ frontend/src/components/ContactUs.js
@@ -2,6 +2,12 @@ import "../Styles/contactus.css";
 import { useState } from "react";
 import axios from "axios";
 
+import { FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { BsPhone } from "react-icons/bs";
+import { CiUser } from "react-icons/ci";
+import { GoMail } from "react-icons/go";
+
 const ContactUs = () => {
   const [alertMessage, setAlertMessage] = useState("");
   const [showAlert, setShowAlert] = useState(false);
@@ -37,13 +43,16 @@ const ContactUs = () => {
   };
 
   return (
-    <div className="conatctus-conatiner">
+    <div className="contactus-container">
+      {/* Alert Section */}
       {showAlert && (
         <div
           className="alert alert-warning alert-dismissible fade show mb-3 d-flex justify-content-between"
           role="alert"
         >
-          <div><strong>Alert: </strong> {alertMessage}</div>
+          <div>
+            <strong>Alert: </strong> {alertMessage}
+          </div>
           <button
             type="button"
             className="close"
@@ -55,69 +64,95 @@ const ContactUs = () => {
           </button>
         </div>
       )}
+
+      {/* Contact us content */}
       <div className="contactus-content">
-        <p>GET IN TOUCH</p>
+        <p className="contactus-content-title">Get in touch!</p>
         <p>
-          Heyo! We are located in montreal. Feel free to use the contact from to
-          the left to reach us out, or write us in the traditional way
+          Heyo! We are located in montreal. <br />
+          Feel free to use the contact form to reach us out.
         </p>
-      </div>
-      <div className="contactus-secondcontent">
-        <form className="contactus-form" onSubmit={submitInquiry}>
-          NAME
-          <input
-            type="text"
-            name="name"
-            value={name}
-            placeholder="Enter your name"
-            onChange={onChange}
-          />
-          EMAIL
-          <input
-            type="text"
-            name="email"
-            value={email}
-            placeholder="Enter your email"
-            onChange={onChange}
-          />
-          PHONE
-          <input
-            type="text"
-            name="phone"
-            value={phone}
-            placeholder="Enter your phone number"
-            onChange={onChange}
-          />
-          MESSAGE
-          <textarea
-            row="5"
-            cols="40"
-            name="message"
-            value={message}
-            onChange={onChange}
+        <div className="contactus-content-links">
+          <a
+            href="geo:45.491303252238524, -73.64341031965458"
+            target="_blank"
+            rel="noreferrer"
           >
-            {" "}
-          </textarea>
-          <input type="submit" value="Send Enquiry" />
-        </form>
-        <div className="contactus-contacts">
-          <div>
-            <p>SEND EMAIL</p>
-            <p>6333 Decarie Bvld,</p>
-            <p>Montreal, QC H3S 1S5</p>
-          </div>
-          <div>
-            <p>ELECTRONIC EMAIL</p>
-            <p>team@hirein.com</p>
-          </div>
-          <div>
-            <p>PHONE SUPPORT</p>
-            <p>Hours: 9am-5pm</p>
-            <p>Monday - Friday</p>
-            <p>+1(123)456-5678</p>
-          </div>
+            <FaMapMarkerAlt className="contactus-content-links-icon" />
+            <span>6333 Bd Décarie</span>
+          </a>
+          <a href="tel:+14388555466" target="_blank" rel="noreferrer">
+            <FaPhoneAlt className="contactus-content-links-icon" />
+            <span>+1 (438) 855-5466</span>
+          </a>
+          <a
+            href="mailto:apurva.mili@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <MdEmail className="contactus-content-links-icon" />
+            <span>apurva.mili@gmail.com</span>
+          </a>
         </div>
       </div>
+      <form className="contactus-form" onSubmit={submitInquiry}>
+        <p className="contactus-form-title">Contact Form</p>
+        <div className="contactus-form-fields-container">
+          <div>
+            <div className="contactus-form-field">
+              <label>Your Name</label>
+              <div className="contactus-form-field-input">
+                <CiUser className="contactus-form-field-input-icon" />
+                <input
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+            <div className="contactus-form-field">
+              <label>Email</label>
+              <div className="contactus-form-field-input">
+                <GoMail className="contactus-form-field-input-icon" />
+                <input
+                  type="text"
+                  name="email"
+                  value={email}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+            <div className="contactus-form-field">
+              <label>Phone</label>
+              <div className="contactus-form-field-input">
+                <BsPhone className="contactus-form-field-input-icon" />
+                <input
+                  type="text"
+                  name="phone"
+                  value={phone}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="contactus-form-field">
+            <label>Message</label>
+            <textarea
+              row="5"
+              cols="40"
+              name="message"
+              value={message}
+              onChange={onChange}
+            ></textarea>
+          </div>
+        </div>
+        <input
+          type="submit"
+          value="Send Enquiry"
+          className="contactus-submit-btn"
+        />
+      </form>
     </div>
   );
 };
